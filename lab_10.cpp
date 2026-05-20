d472c2d65e6783852a6c817130050a39efd6baf5
murmelow <waterproofed@bk.ru>
2026-05-20 11:57:06 +0300

    Add files via upload

---
+++ lab_10.cpp
@@ -0,0 +1,17 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+
+int main(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    lab10::MyForm form;
+    Application::Run(% form);
+
+    return 0;
+}
